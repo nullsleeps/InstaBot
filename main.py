fa51d5efeff363f0f40faf9ae5b49bccf09ff653
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 10:37:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from instapy import instapy
+from instapy import InstaPy
 
-
-session = instapy(username = "username", password = "password")
-# In the username section put you're instagram username there and in the password section put your instagram password
+#Enter your iInstagram Login Details
+session = InstaPy(username="your_username", password="your_password")
 session.login()
-session.set_relationship_bounds(enabled = True, max_followers = 300)
-#change the max followers to the amount you want
-session.set_do_follow(True, percentage = 100)
-session.like_by_tags(["Unix", "Linux", "Python", "BSD", "MacOS", "Kernel"], amount = 4)
-#change the tags to what you want to like and comment on
-session.set_dont_like(["Windows", "Microsoft"])
-#change these tags to what you don't want to like and comment on
+
+
+session.set_relationship_bounds(enabled=True, max_followers=300)
+
+# Enable liking and following
+session.set_do_follow(True, percentage=100)
+session.set_do_like(True, percentage=100)
+
+# Like posts by specific tags
+session.like_by_tags(["Cats", "Cars", "Memes", "Funny", "Drift", "Rich"], amount=4)
+
+# Exclude certain tags
+session.set_dont_like(["News", "Girls"])
+
+
+session.end()
