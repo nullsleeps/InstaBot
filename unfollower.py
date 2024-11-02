7ff9523170007afe9515c056cd350c3185253891
null <68474200+nullsleeps@users.noreply.github.com>
2024-11-02 10:38:29 -0400

    Update unfollower.py

--- unfollower.py
+++ unfollower.py
@@ -1,10 +1,10 @@
-#this one unfollows a certain amount of people you want to unfollow
-from instapy import instapy
+from instapy import InstaPy
 
-
-session = instapy(username = "username", password = "password")
-# In the username section put you're instagram username there and in the password section put your instagram password
+# Enter Your Instagram Login Details.
+session = InstaPy(username="your_username", password="your_password")
 session.login()
 
-session.unfollow_users(amount = 10, allFollowing = True, sleep_delay = 20)
-#change the amount (10 by defualt) to the amount of people you want to unfollow
+# Unfollow users
+session.unfollow_users(amount=10, allFollowing=True, sleep_delay=20)
+
+session.end()
